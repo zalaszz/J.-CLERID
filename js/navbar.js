0f7840bebcd0da01aee255b11a883c61e7405667
zαlαsz <rickysilva2002@hotmail.com>
2022-05-06 03:41:53 +0100

    update 1.3

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+document.write(`
+    <nav>
+        <ul>
+            <!-- <li> <img src = "img/J.CLERID.jpg" id="logo" width="85" height="65"> </a></li> -->
+            <li class="navbar-name"><a href="index.html"><span>J. CLERID</span> SOLUTIONS</a></li>
+            <li><a href="index.html"><i class="fa-solid fa-house"></i> Home </a></li>
+            <li><a href="about.html"><i class="fa-solid fa-circle-question"></i> About Us </a></li>
+            <li><a href="services.html"><i class="fa-solid fa-bars-progress"></i> Services </a></li>
+            <li><a href="contact.html"><i class="fa-solid fa-phone"></i> Contact Us </a></li>
+        </ul>
+    </nav>
+`);
